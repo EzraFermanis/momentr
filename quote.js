032bed14bcf10d5a5b8471b63b4a0f93948125b0
Ezra Fermanis <ezra.fermanis@gmail.com>
2016-04-21 16:01:28 +1200

    presented product

---
+++ quote.js
@@ -0,0 +1,15 @@
+// var request = require("superagent")
+// // var dotenv = require("dotenv")
+// var fs = require("fs")
+//
+//
+// function quote() {
+//   request.get("http://quotes.rest/qod.json",function(req,res){
+//     // fs.writeFile("./data.json",JSON.parse(res.text).items[0].volumeInfo.description,"utf8")
+//     var data = res.body.contents.quotes[0].quote
+//   })
+// }
+//
+//
+//
+// module.exports = quote;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
+// var quote = require('../quote')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Momentr' });
+});
+
+// Get the 'before' moments page //
+router.get('/beforegame', function(req, res, next){
+  res.render('beforegame', {title: "Momentr thinks this is what you need before your moment"} );
+  // res.render('quote', {theQuote: quote});
+});
+
+router.get('/beforedate', function(req, res, next){
+  res.render('beforedate', {title: "Momentr thinks this is what you need before your moment"} );
+});
+
+router.get('/beforespeech', function(req, res, next){
+  res.render('beforespeech', {title: "Momentr thinks this is what you need before your moment"} );
+});
+
+
+router.get('/afterhardday', function(req, res, next){
+  res.render('afterhardday', {title: "Momentr thinks this is what you need after your moment"} );
+});
+
+router.get('/afterbreakup', function(req, res, next){
+  res.render('afterbreakup', {title: "Momentr thinks this is what you need after your moment"} );
+});
+
+router.get('/afterdrinks', function(req, res, next){
+  res.render('afterdrinks', {title: "Momentr thinks this is what you need after your moment"} );
 });
 
 module.exports = router;
